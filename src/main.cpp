4f04fc6eeb3f9417808bcd3143b170690e74a229
Steve <stevefrazee123@gmail.com>
2018-11-12 19:43:08 -0500

    tune gain values

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.008, 0.0, 0.2);
+  pid.Init(0.25, 0.0005, 3.5);
   
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,9 +58,10 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
 	  pid.UpdateError(cte);
 	  steer_value = pid.TotalError();
-          
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
